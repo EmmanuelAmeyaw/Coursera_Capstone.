1ba091d3b1c5236f37480bdcade32f9c83c16488
EmmanuelAmeyaw <48012593+EmmanuelAmeyaw@users.noreply.github.com>
2019-06-12 08:52:37 +0000

    capstone project
    capstone project

---
+++ ameyaw_emmanuel_capstone_project.py
@@ -0,0 +1,24 @@
+
+# coding: utf-8
+
+# # Capstone project
+
+# ## This notebook will mainly be used for a capstone project
+
+# In[1]:
+
+
+import pandas as pd
+
+
+# In[2]:
+
+
+import numpy as np
+
+
+# In[3]:
+
+
+print('Hello Capstone Project Course!')
+
